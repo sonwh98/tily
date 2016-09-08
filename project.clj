4e8f0de0ce486605982b44cb487548a8299c3e61
sto <Son.C.To@gmail.com>
2016-09-07 23:01:36 -0500

    add core.async cljsjs.moment

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject  com.kaicode/tily "0.1.6-SNAPSHOT"
-  :source-paths ["src"])
+  :source-paths ["src"]
+  :dependencies [[cljsjs/moment "2.10.6-4"]
+                 [org.clojure/core.async "0.2.385"]])
