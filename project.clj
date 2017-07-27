5681b2fccb41f1a8eb4cbd9942dd0bb8f7c1c199
Sonny To <son.c.to@gmail.com>
2017-07-27 11:58:47 -0400

    upgrade core.async

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject  com.kaicode/tily "0.1.6-SNAPSHOT"
   :source-paths ["src"]
   :dependencies [[cljsjs/moment "2.10.6-4"]
-                 [org.clojure/core.async "0.2.395"]])
+                 [org.clojure/core.async "0.3.443"]
+                 ])
