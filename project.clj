b6f4fcc9dab81707106e96dae8081f1d38287772
sto <son.c.to@gmail.com>
2019-05-16 00:56:15 +0800

    add clojure 1.10 so that cider doesnt complain about clojure 1.7

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject  stigmergy/tily "0.1.7-SNAPSHOT"
   :source-paths ["src"]
-  :dependencies [[cljsjs/moment "2.24.0-0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cljsjs/moment "2.24.0-0"]
                  [org.clojure/core.async "0.4.490"]
                  ])
