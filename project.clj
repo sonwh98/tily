7f0ef06908c1645b955a7a49f28a01b8092938be
sto <son.c.to@gmail.com>
2019-05-18 15:10:25 +0800

    change source-path

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject  stigmergy/tily "0.1.7-SNAPSHOT"
-  :source-paths ["src"]
+(defproject  stigmergy/tily "0.1.8-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cljsjs/moment "2.24.0-0"]
-                 [org.clojure/core.async "0.4.490"]
-                 ])
+                 [org.clojure/core.async "0.4.490"]]
+  
+  :source-paths ["src/cljs" "src/cljc"]
+  )
